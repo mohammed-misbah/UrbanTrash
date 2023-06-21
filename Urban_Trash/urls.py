7af0a4e24c61983bcdca2f6650a8c5a4eabce66a
mohammed-misbah <115168697+mohammed-misbah@users.noreply.github.com>
2023-06-21 14:52:03 +0530

    Updated Status

--- Urban_Trash/urls.py
+++ Urban_Trash/urls.py
@@ -30,9 +30,9 @@ urlpatterns = [
     path("adminapi/",include('WasteCategory.urls')),
     path('adminapi/',include('AdminSide.urls')),
     path('api/',include('notification.urls')),
-    # path('api-auth/',include('rest_framework.urls')),
     path("api/schema/",SpectacularAPIView.as_view(),name="schema"),
     path("api/schema/docs/",SpectacularSwaggerView.as_view(url_name="schema")),
+    # path('api-auth/',include('rest_framework.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- notification/urls.py
+++ notification/urls.py
@@ -1,22 +1,22 @@
-from django.urls import path
-# from notification import api
-from .unifonic_channel import UnifonicChannel, UnifonicMessage
-from .notification import HasDatabaseNotifications, RoutesNotifications
-from .notification_sender import NotificationSender
-# from notifiable import HasDatabaseNotifications, RoutesNotifications
-from .mail_channel import MailChannel
-from .database_channel import DatabaseChannel
-from .base_channel import BaseChannel
+# from django.urls import path
+# # from notification import api
+# from .unifonic_channel import UnifonicChannel, UnifonicMessage
+# from .notification import HasDatabaseNotifications, RoutesNotifications
+# from .notification_sender import NotificationSender
+# # from notifiable import HasDatabaseNotifications, RoutesNotifications
+# from .mail_channel import MailChannel
+# from .database_channel import DatabaseChannel
+# from .base_channel import BaseChannel
 
 
 
-urlpatterns = [
-    path('unifonichannel/',UnifonicChannel,name='unofonic-channel'),
-    path('unifonicmessage/',UnifonicMessage, name='unifonic-message'),
-    path('databasenotification/',HasDatabaseNotifications,name='database-notification'),
-    path('routenotification/',RoutesNotifications,name='route-notification'),
-    path('notificationsender/',NotificationSender,name='notification-sender'),
-    path('mailchannel/',MailChannel,name='mail-channel'),
-    path('databasechannel/',DatabaseChannel,name='database-channel'),
-    path('basechannel/',BaseChannel,name='base-channel'),
-]
+# urlpatterns = [
+#     path('unifonichannel/',UnifonicChannel,name='unofonic-channel'),
+#     path('unifonicmessage/',UnifonicMessage, name='unifonic-message'),
+#     path('databasenotification/',HasDatabaseNotifications,name='database-notification'),
+#     path('routenotification/',RoutesNotifications,name='route-notification'),
+#     path('notificationsender/',NotificationSender,name='notification-sender'),
+#     path('mailchannel/',MailChannel,name='mail-channel'),
+#     path('databasechannel/',DatabaseChannel,name='database-channel'),
+#     path('basechannel/',BaseChannel,name='base-channel'),
+# ]
